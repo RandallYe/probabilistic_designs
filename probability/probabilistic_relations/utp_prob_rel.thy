109096b199bf89e2e7445868ccb818e533dff97d
rye <ye.randall@gmail.com>
2022-08-23 00:25:42 +0100

    utp_prob_rel_lattice.thy

---
+++ probability/probabilistic_relations/utp_prob_rel.thy
@@ -0,0 +1,9 @@
+section \<open> The Hehner's predicative probabilistic programming in UTP \<close>
+
+theory utp_prob_rel
+  imports 
+    utp_iverson_bracket 
+    utp_distribution 
+    utp_prob_rel_lattice 
+    utp_prob_rel_lattice_laws
+begin end
